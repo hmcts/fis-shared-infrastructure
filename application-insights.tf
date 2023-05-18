df7784d3e5d66dc6e7c3178a86149d3b41de4a7d
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-05-18 23:15:13 +0100

    Update application-insights.tf

--- application-insights.tf
+++ application-insights.tf
@@ -20,3 +20,10 @@ resource "azurerm_key_vault_secret" "appInsights-InstrumentationKey" {
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.fis-kv-vault.key_vault_id
 }
+  
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.key-vault.key_vault_id
+}
+

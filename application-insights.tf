f5529590707d8edfdaca493af2956f398bb22ff4
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2025-09-26 14:52:00 +0000

    Updating Terraform Formatting

--- application-insights.tf
+++ application-insights.tf
@@ -3,7 +3,7 @@ module "application_insights" {
 
   env      = var.env
   product  = var.product
-  name     = "${var.product}"
+  name     = var.product
   location = var.appinsights_location
 
   resource_group_name = azurerm_resource_group.rg.name
--- key-vault.tf
+++ key-vault.tf
@@ -1,13 +1,13 @@
 module "fis-kv-vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                       = "${var.product}-kv-${var.env}"
-  product                    = var.product
-  env                        = var.env
-  tenant_id                  = var.tenant_id
-  object_id                  = var.jenkins_AAD_objectId
-  resource_group_name        = azurerm_resource_group.rg.name
-  product_group_name         = "DTS Family Integration"
-  common_tags                = var.common_tags
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                = "${var.product}-kv-${var.env}"
+  product             = var.product
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
+  product_group_name  = "DTS Family Integration"
+  common_tags         = var.common_tags
 
   create_managed_identity              = true
   additional_managed_identities_access = var.additional_managed_identities_access
